a4ac3d4036048ea17853d5b6ea82479f0f313792
ravi7317 <ravikurmi8313@gmail.com>
2025-11-08 12:26:02 +0530

    String Print , scanner,

--- CWH/string.java
+++ CWH/string.java
@@ -1,9 +1,21 @@
 package CWH;
+
+import java.util.Scanner;
+
 public class string {
     public static void main(String[] args) {
-        {
-        String name = "Harry";
-        System.out.println(name);
-    }
+        
+       // String name = new String ("Harry");
+
+        int a = 6;
+        float b = 5.6f;
+      //  System.out.printf("The value of a is %d and the value of  b is %f ",a,b);
+       // System.out.format("The value of a is %d and the value of  b is %f ",a,b);
+       // System.out.println(name);
+       Scanner sc = new Scanner(System.in);
+       String st = sc.nextLine();
+       System.out.println(st);
+
+    
 }
 }
