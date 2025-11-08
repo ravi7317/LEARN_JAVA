745304f5c840494bbb5d9f9cc46c487848fa01b2
ravi7317 <ravikurmi8313@gmail.com>
2025-11-08 20:05:09 +0530

    Add initial implementation of ifelse class with age driving eligibility check

---
+++ IfElse/ifelse.java
@@ -0,0 +1,13 @@
+package IfElse;
+public class ifelse {
+    public static void main(String[] args) {
+        int age =34;
+        if(age>18){
+            System.out.println("yes you can drive");
+
+        }
+        else{
+            System.out.println("you can not drive");
+        }
+    }
+}
